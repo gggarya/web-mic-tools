75a969be014969cf977c20c540c6a7733af502cf
gggarya <arya@m78.com>
2026-06-22 19:19:50 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+
+const CACHE_NAME = 'mic2fft-v0.1';
+
+const FILES = [
+  './',
+  './mic2fft.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
